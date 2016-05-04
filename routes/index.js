0ad1c11f11da8207469312ee4c3bc30b0fa4cd4c
MatMath <mathieu.k.legault@gmail.com>
2016-05-04 13:04:40 -0400

    Basic API info structure working. todo: make it cuter.

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,18 @@ var router = express.Router();
 
 router.get('/', function(req, res) {
 	var data = {
-		title: 'Mathieu',
-		quality: "Awesomeness",
-		location: req.location
+		title: 'API possible routes',
+		location: req.location,
+		possibleRoutes: [{
+			url: '/buildings',
+			description: 'List of possible building type from the system.'
+		},{
+			url: '/intrates',
+			description: 'List of the current Interest rate offer globally, 5, 15, 30 years fix and variable.'
+		},{
+			url: '/medRentOfArea/:areaCode',
+			description: 'With the neighborhood code given return the median rent for the area.'
+		}]
 	};
 
 	res.render('index', data);
